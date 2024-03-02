ac063f5f7a01c000255b447b5d1f2914b0b734fa
Alfred Ochieng <techyobura@gmail.com>
2024-03-02 10:24:35 +0300

    user model

---
+++ api/modules/user.module.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
